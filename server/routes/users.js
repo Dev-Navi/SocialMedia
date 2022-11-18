7d8d4e6fe2b44da9a1c0503d8de1b8a97ccbf269
naveenkumar <naveenkumar@wealwim.com>
2022-11-18 19:06:22 +0530

    updatecommit

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Update User
 router.put("/:id", async (req, res) => {
-    console.log(req.body.userId, req.param.id);
+  console.log(req.body.userId, req.param.id);
   try {
     if (req.body.userId === req.params.id || req.body.isAdmin) {
       if (req.body.password) {
@@ -29,38 +29,62 @@ router.put("/:id", async (req, res) => {
         return res.status(500).json({ error: "Some error on server" });
       }
     } else {
-        return res.status(400).json({ error: "You can update only your account" });
+      return res
+        .status(400)
+        .json({ error: "You can update only your account" });
     }
   } catch (err) {}
 });
 
 // Delete User
 router.delete("/:id", async (req, res) => {
-    console.log(req.body.userId, req.params.id);
-    if (req.body.userId === req.params.id || req.body.isAdmin) {
-      try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        return res.status(200).json({ msg: "Account has been Deleted" });
-      } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Some error on server" });
-      }
-    } else {
-      res.status(400).json({ error: "You can Deleted only your account" });
+  console.log(req.body.userId, req.params.id);
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json({ msg: "Account has been Deleted" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Some error on server" });
     }
-
+  } else {
+    res.status(400).json({ error: "You can Deleted only your account" });
+  }
 });
 
 // Get User
-router.get("/:id", async (req,res)=>{
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, createdAt, ...other } = user._doc;
+    return res.status(200).json({ msg: "Your Details is", user: other });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Some error on server" });
+  }
+});
+
+//  Follow User
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
     try {
-        const user = await User.findById(req.params.id);
-        const {password, updatedAt, createdAt, ...other} = user._doc
-        return res.status(200).json({ msg: "Your Details is", user:other });
+      const user = await User.findById(req.params.id);
+      const currentuser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentuser.updateOne({ $push: { following: req.body.userId } });
+        res.status(200).json({ msg: "User has been followed" });
+      } else {
+        res.status(400).json({ error: "You already follow this user" });
+      }
     } catch (err) {
         console.log(err);
-        res.status(500).json({ error: "Some error on server" });
+        res.status(400).json({ error: "Server Error" });
     }
-})
+  } else {
+    res.status(400).json({ error: "You con't follow your self" });
+  }
+});
 
 export default router;
